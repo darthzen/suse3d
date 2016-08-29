0b0676f0effb6974b283e5810ab95e1906833822
Rick Ashford <rick@theashfords.org>
2016-08-28 23:18:47 -0500

    Fixed typo in urls.py

--- urls.py
+++ urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
         url(r'^manager/', include('manager.urls')),
-        url(r'^admin/', admin.stie.urls),
+        url(r'^admin/', admin.site.urls),
 ]        
